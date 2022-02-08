79079131f95fbb094381198eff35609646324dff
jmasonlee <jmasonlee@gmail.com>
2022-02-08 16:24:43 -0700

    first commit

---
+++ model_objects.py
@@ -0,0 +1,48 @@
+from enum import Enum
+
+
+class Product:
+    def __init__(self, name, unit):
+        self.name = name
+        self.unit = unit
+
+    def __repr__(self):
+        return f"{self.name}  {self.unit}"
+
+
+class ProductQuantity:
+    def __init__(self, product, quantity):
+        self.product = product
+        self.quantity = quantity
+
+
+class ProductUnit(Enum):
+    EACH = 1
+    KILO = 2
+
+
+class SpecialOfferType(Enum):
+    THREE_FOR_TWO = 1
+    TEN_PERCENT_DISCOUNT = 2
+    TWO_FOR_AMOUNT = 3
+    FIVE_FOR_AMOUNT = 4
+
+class Offer:
+    def __init__(self, offer_type, product, argument):
+        self.offer_type = offer_type
+        self.product = product
+        self.argument = argument
+
+
+class Discount:
+    def __init__(self, product, description, discount_amount):
+        self.product = product
+        self.description = description
+        self.discount_amount = discount_amount
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return f"{self.product=} {self.description=} {self.discount_amount=}"
+
---
+++ tests/test_supermarket.py
@@ -0,0 +1,78 @@
+import pytest
+
+from model_objects import Product, SpecialOfferType, ProductUnit, Discount
+from shopping_cart import ShoppingCart
+from teller import Teller
+from tests.fake_catalog import FakeCatalog
+from approvaltests import verify
+
+
+def test_ten_percent_discount():
+    apples, teller, toothbrush = create_teller_and_items()
+    teller.add_special_offer(SpecialOfferType.TEN_PERCENT_DISCOUNT, toothbrush, 10.0)
+
+    cart = ShoppingCart()
+    cart.add_item_quantity(apples, 2.5)
+    cart.add_item_quantity(toothbrush, 1)
+
+    receipt = teller.checks_out_articles_from(cart)
+
+    verify(str(receipt))
+
+
+def test_full_receipt_no_discount():
+    apples, teller, toothbrush = create_teller_and_items()
+
+    cart = ShoppingCart()
+    cart.add_item_quantity(apples, 2.5)
+    cart.add_item_quantity(toothbrush, 1)
+
+    receipt = teller.checks_out_articles_from(cart)
+
+    verify(str(receipt))
+
+
+def test_full_receipt_add_item_adds_one_item():
+    apples, teller, toothbrush = create_teller_and_items()
+
+    cart = ShoppingCart()
+    cart.add_item(apples)
+    cart.add_item(toothbrush)
+
+    receipt = teller.checks_out_articles_from(cart)
+
+    verify(str(receipt))
+
+
+def test_product_quantities_updates():
+    apples, teller, toothbrush = create_teller_and_items()
+
+    cart = ShoppingCart()
+    cart.add_item(apples)
+    cart.add_item_quantity(toothbrush, 2.0)
+    cart.add_item(toothbrush)
+
+    receipt = teller.checks_out_articles_from(cart)
+    verify(str(cart.product_quantities) + str(receipt))
+
+def test_full_receipt_three_for_two():
+    apples, teller, toothbrush = create_teller_and_items()
+    teller.add_special_offer(SpecialOfferType.THREE_FOR_TWO, toothbrush, "foor")
+
+    cart = ShoppingCart()
+    cart.add_item_quantity(apples, 2.5)
+    cart.add_item_quantity(toothbrush, 1)
+
+    receipt = teller.checks_out_articles_from(cart)
+
+    verify(str(receipt))
+
+
+def create_teller_and_items():
+    catalog = FakeCatalog()
+    toothbrush = Product("toothbrush", ProductUnit.EACH)
+    catalog.add_product(toothbrush, 0.99)
+    apples = Product("apples", ProductUnit.KILO)
+    catalog.add_product(apples, 1.99)
+    teller = Teller(catalog)
+    return apples, teller, toothbrush
