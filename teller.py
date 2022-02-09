1f5ee6de72391bf1579fc50db460fa87fae1cdb7
jmasonlee <jmasonlee@gmail.com>
2022-02-08 17:45:00 -0700

    r Part 2 step 1: Extract method on the juice part

--- teller.py
+++ teller.py
@@ -13,6 +13,14 @@ class Teller:
 
     def checks_out_articles_from(self, the_cart):
         #Begin Common Code
+        receipt = self.common_code(the_cart)
+        #End Common Code Note: handle_offers could also contain common code
+
+        the_cart.handle_offers(receipt, self.offers, self.catalog)
+
+        return receipt
+
+    def common_code(self, the_cart):
         receipt = Receipt()
         product_quantities = the_cart.items
         for pq in product_quantities:
@@ -20,10 +28,6 @@ class Teller:
             quantity = pq.quantity
             unit_price = self.catalog.unit_price(p)
             price = quantity * unit_price
-            #price = 0.00 #quantity * unit_price
+            # price = 0.00 #quantity * unit_price
             receipt.add_product(p, quantity, unit_price, price)
-        #End Common Code Note: handle_offers could also contain common code
-
-        the_cart.handle_offers(receipt, self.offers, self.catalog)
-
         return receipt
