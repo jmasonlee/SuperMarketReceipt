8270bbe010c2c68dc02f9ea30ca9af259faaae64
jmasonlee <jmasonlee@gmail.com>
2022-02-09 16:18:13 -0700

    t Part 2 step 3: Create test for the juice part
    Add test for item not in catalog

--- tests/test_common_code.py
+++ tests/test_common_code.py
@@ -1,5 +1,7 @@
+import pytest
 from approvaltests import verify
 
+from model_objects import Product, ProductUnit
 from shopping_cart import ShoppingCart
 from teller import Teller
 from tests.fake_catalog import FakeCatalog
@@ -8,3 +10,17 @@ from tests.fake_catalog import FakeCatalog
 def test_common_code_empty_catalog():
     teller = Teller(FakeCatalog())
     verify(teller.common_code(ShoppingCart()))
+
+
+def test_item_not_in_catalog():
+    teller = Teller(FakeCatalog())
+
+    cart = ShoppingCart()
+    cart.add_item(Product("toothbrush", ProductUnit.EACH))
+
+    with pytest.raises(KeyError) as exception:
+        teller.common_code(cart)
+    verify(exception)
+
+#catalog with toothbrush and apples, checkout toothbrush and apples
+#catalog with toothbrush and apples, checkout apples
