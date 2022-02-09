30be9cbddf02c02dd408ee34abf2afd3e16d2599
jmasonlee <jmasonlee@gmail.com>
2022-02-08 17:33:35 -0700

    B  Preparation:  Revert your change By Fixing Bug

--- teller.py
+++ teller.py
@@ -18,8 +18,8 @@ class Teller:
             p = pq.product
             quantity = pq.quantity
             unit_price = self.catalog.unit_price(p)
-            #price = quantity * unit_price
-            price = 0.00 #quantity * unit_price
+            price = quantity * unit_price
+            #price = 0.00 #quantity * unit_price
             receipt.add_product(p, quantity, unit_price, price)
 
         the_cart.handle_offers(receipt, self.offers, self.catalog)
